804bdc0aa885045e9be162c020bc0f16e153aec9
Duy Bản IT <69631089+nguyenduyban@users.noreply.github.com>
2022-11-10 08:50:57 +0700

    Update and rename thctdl.cpp to de_2thctdl.cpp

---
+++ de_2thctdl.cpp
@@ -0,0 +1,259 @@
+//THƯ VIỆN
+
+#include <iostream>
+using namespace std;
+const int MAX = 50;
+struct DAYSO
+{
+	int n;
+	int data[MAX];
+};
+void nhap(DAYSO &ds);
+void xuat(DAYSO ds);
+void xoa1PT(DAYSO& ds, int pos);
+void thayThe(DAYSO& ds, int pos, int x);
+typedef int TYPEINFO;
+struct NODE
+{
+	TYPEINFO data;
+	NODE* link;
+};
+typedef NODE* NODEPTR;
+void khoiTao(NODEPTR& list);
+int isEmpty(NODEPTR list);
+NODEPTR tao_NODE(TYPEINFO x);
+NODEPTR them_Dau(NODEPTR& list, TYPEINFO x);
+void nhap_DS(NODEPTR& list);
+void xuat_DS(NODEPTR list);
+void xoadau(NODEPTR& list);
+void tongChan(NODEPTR list);
+void xuat_PT_Am(NODEPTR list);
+void xuat_PT_X(NODEPTR list, TYPEINFO x);
+void giaiPhong(NODEPTR& list);
+
+// Cài đặt
+#include "Header.h"
+void nhap(DAYSO& ds)
+{
+	cout << "\nNhap so luong phan tu : ";
+	cin >> ds.n;
+	cout << "\nNhap ds ";
+	for (int i = 0; i < ds.n; i++)
+	{
+		cout << "\nNhap ds[ " << i << " ]: ";
+		cin >> ds.data[i];
+	}
+}
+void xuat(DAYSO ds)
+{
+	for (int i = 0; i < ds.n; i++)
+		cout << ds.data[i] << " ";
+}
+void xoa1PT(DAYSO& ds, int pos)
+{
+
+	for (int i = 0; i < ds.n; i++)
+	{
+		if (i == pos)
+			for (int j = pos; j < ds.n; j++)
+				ds.data[j] = ds.data[j + 1];
+	}
+	ds.n--;
+}
+void thayThe(DAYSO& ds, int pos, int x)
+{
+	for (int i = 0; i < ds.n; i++)
+	{
+		if (i == pos)
+			ds.data[i] = x;
+	}
+}
+
+void khoiTao(NODEPTR& list)
+{
+	list = NULL;
+}
+int isEmpty(NODEPTR list)
+{
+	return(list == NULL ? 1 : 0);
+}
+NODEPTR tao_NODE(TYPEINFO x)
+{
+	NODEPTR p;
+	p = new NODE;
+	p->data = x;
+	p->link = NULL;
+	return p;
+}
+NODEPTR them_Dau(NODEPTR& list, TYPEINFO x)
+{
+	NODEPTR p;
+	p = tao_NODE(x);
+	p->link = list;
+	list = p;
+	return p;
+}
+void nhap_DS(NODEPTR& list)
+{
+	khoiTao(list);
+	int x;
+	do {
+		cout << "\nNhap vao x (thoat -99): ";
+		cin >> x;
+		if (x == -99)
+			break;
+		them_Dau(list, x);
+	} while (1);
+}
+void xuat_DS(NODEPTR list)
+{
+	NODEPTR p = list;
+	while (p != NULL)
+	{
+		cout << p->data << " ";
+		p = p->link;
+	}
+}
+bool timX(NODEPTR list, TYPEINFO x)
+{
+	NODEPTR p = list;
+	while (p != NULL)
+	{
+		if (p->data == x)
+			return true;
+		p = p->link;
+	}
+	return false;
+}
+void xoadau(NODEPTR& list)
+{
+	NODEPTR p;
+	if (list != NULL)
+	{
+		p = list;
+		list = p->link;
+		delete p;
+	}
+}
+void xoa_X(NODEPTR& list, TYPEINFO x)
+{
+	NODEPTR before;
+	NODEPTR p;
+	if (timX(list, x) == false)
+		cout << "\nKhong co ptu " << x << " trong DS ";
+	else
+	{
+		cout << "\nDSLK sau khi xoa " << x << endl;
+
+		while (timX(list, x) == true)
+		{
+			if (list->data == x)
+				xoadau(list);
+			else
+			{
+				p = list;
+				before = list;
+				while ((p != NULL) && (p->data != x))
+				{
+					before = p;
+					p = p->link;
+				}
+				if (p != NULL)
+				{
+					before->link = p->link;
+					delete p;
+				}
+			}
+		}
+	}
+}
+
+void tongChan(NODEPTR list)
+{
+	NODEPTR p = list;
+	int tong = 0;
+	while (p != NULL)
+	{
+		if (p->data % 2 == 0)
+			tong += p->data;
+		p = p->link;
+	}
+	cout << "\nTong cac ptu chan trong DS : " << tong;
+}
+void xuat_PT_X(NODEPTR list, TYPEINFO x)
+{
+	NODEPTR p = list;
+	int dem = 0;
+	while (p != NULL)
+	{
+		if (p->data == x)
+			cout << "\nVi tri : " << dem;
+		p = p->link;
+		dem++;
+	}
+	if (dem == 0)
+		cout << "\nKhong co gtri " << x;
+}
+void xuat_PT_Am(NODEPTR list)
+{
+	NODEPTR p = list;
+	while (p != NULL)
+	{
+		if (p->data < 0)
+			cout << p->data << " ";
+		p = p->link;
+	}
+}
+void giaiPhong(NODEPTR& list)
+{
+	NODEPTR p = list;
+	while (p != NULL)
+	{
+		list = list->link;
+		delete p;
+		p = list;
+	}
+}
+
+// Chương Trình
+#include"Header.h"
+int main()
+{
+	DAYSO a;
+	nhap(a);
+	cout << "\nDay so vua nhap la\n";
+	xuat(a);
+	int pos, x;
+	cout << "\nNhap vi tri can xoa : ";
+	cin >> pos;
+	xoa1PT(a, pos);
+	cout << "\nDay so sau khi xoa ";
+	xuat(a);
+	cout << "\nNhap vi tri can thay the ";
+	cin >> pos;
+	cout << "\nNhap ptu can thay the ";
+	cin >> x;
+	thayThe(a, pos, x);
+	cout << "\nDay so sau khi thay the ";
+	xuat(a);
+	cout << "\nNhap vi tri can chen them ";
+	cin >> pos;
+	cout << "\nNhap ptu can chen them ";
+	cin >> x;
+	xuat(a);
+	cout << "\nNhap vi tri can xuat ";
+	cin >> pos;
+
+	NODEPTR l;
+	khoiTao(l);
+	nhap_DS(l);
+	cout << "\nDSLK vua nhap ";
+	xuat_DS(l);
+	tongChan(l);
+	cout << "\nCac phan tu am trong DS ";
+	xuat_PT_Am(l);
+	cout << "\nNhap phan tu can xoa ";
+	cin >> x;
+	xuat_DS(l);
+	giaiPhong(l);
+}
